0455accff8f1d55a75b7309148217995756554c0
n3wjack <n3wjack@n3wjack.net>
2013-07-07 12:22:59 +0200

    updated footer with static theme link

--- footer.php
+++ footer.php
@@ -18,7 +18,7 @@
 			<?php do_action( 'twentytwelve_credits' ); ?>
 			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentytwelve' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentytwelve' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'twentytwelve' ), 'WordPress' ); ?></a>
 			
-			<span class="credits"><a href="http://n3wjack.net/" title="Twenty Twelve Options" target="_blank">Twenty Twelve Evening Red</a></span>
+			<span class="credits"><a href="http://n3wjack.net/twentytwelvedarktheme/" title="Twenty Twelve Options" target="_blank">Twenty Twelve Evening Red</a></span>
 				<br>
 			
 		</div><!-- .site-info -->
