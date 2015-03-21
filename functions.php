09669d32ff750aee72dd93c57fcc6e04249d6aae
Sebastian Rothe <creeper_si@yahoo.de>
2015-03-21 14:04:44 +0700

    changed inclusion of parent stylesheet
    recommended solution via http://codex.wordpress.org/Child_Themes

--- functions.php
+++ functions.php
@@ -1,5 +1,15 @@
 <?php
 
+// include parent stylesheet
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array('parent-style')
+    );
+}
+
 // Use WP-PageNavi when it's active
 function twentytwelve_content_nav( $nav_id ) {
 	global $wp_query;
