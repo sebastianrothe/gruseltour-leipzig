c1144676aaa75b62db6fefe1a9ef968f0f7517fa
Sebastian Rothe <creeper_si@yahoo.de>
2015-03-21 13:55:57 +0700

    fixed indention

--- footer.php
+++ footer.php
@@ -13,14 +13,12 @@
 	</div><!-- #main .wrapper -->
 	<footer id="colophon" role="contentinfo">
 
-
 		<div class="site-info">
 			<?php do_action( 'twentytwelve_credits' ); ?>
 			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentytwelve' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentytwelve' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'twentytwelve' ), 'WordPress' ); ?></a>
 			
 			<span class="credits"><a href="http://n3wjack.net/twentytwelvedarktheme/" title="Twenty Twelve Options" target="_blank">Twenty Twelve Evening Red</a></span>
-				<br>
-			
+			<br>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
