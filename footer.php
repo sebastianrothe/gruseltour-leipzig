c2e9e0fe7211e0888068f6902e1009cc0b0e82fb
Sebastian Rothe <creeper_si@yahoo.de>
2015-03-26 13:08:46 +0700

    removed booking button from booking page

--- footer.php
+++ footer.php
@@ -10,20 +10,21 @@
  * @since Twenty Twelve 1.0
  */
 ?>
-	<!-- Mail Button -->
+		<!-- Mail Button -->
 
-	<!-- Telefon Button -->
+		<!-- Telefon Button -->
 
-	<!-- Buchen Button -->
-	<div class="buchen-div">
-		<a class="buchen-button" href="//gruseltour-leipzig.de/anmeldung">Gruseltour hier buchen!</a>
-	</div>
-
+		<!-- Buchen Button -->
+		<?php if (!is_page('anmeldung')) { ?>
+		<div class="buchen-div">
+			<a class="buchen-button" href="//gruseltour-leipzig.de/anmeldung">Gruseltour hier buchen!</a>
+		</div>
+		<?php } ?>
 	</div><!-- #main .wrapper -->
+
 	<footer id="colophon" role="contentinfo">
 		<div class="site-info">
 			<a href="//gruseltour-leipzig.de/impressum">Impressum</a>
-			<br />
 			<br />
 			Gruseltour-Leipzig Proudly powered by <a href="http://wordpress.org/" title="Wordpress">Wordpress</a>. Theme: Twenty Twelve Dark Theme by <a href="http://www.zeaks.org/2012/twenty-twelve-dark-child-theme">Zeaks</a> 
 		</div><!-- .site-info -->
