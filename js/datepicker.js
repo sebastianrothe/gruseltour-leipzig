1fc3fbc7c5a47490e501b7a1b043a2f7342c1cd4
Sebastian Rothe <creeper_si@yahoo.de>
2015-03-27 13:43:35 +0700

    added function to load data file

--- js/datepicker.js
+++ js/datepicker.js
@@ -31,7 +31,18 @@ function isAvailableTourDate(date) {
     return [true];
 }
 
-// TODO: outsource these dates
+function loadData(filename) {
+	jQuery.get(filename, function(data) {
+		return transformDateLinesToArray(data);
+	});
+}
+
+function transformDateLinesToArray(lines) {
+	// clean, split and parseToGerman
+	return cleanDisabledDateString(lines).split("\n").map(toGermanStringWithZeros);
+}
+
 /* an diesen Tagen können keine Termine mehr angenommen werden */
-var disabledTourDays = ["27.3.2015","28.3.2015","21.3.2015","10.4.2015","11.4.2015","12.4.2015","4.4.2015","5.4.2015","17.4.2015","25.4.2015","26.4.2015","29.5.2015","30.5.2015","31.5.2015","3.7.2015","4.7.2015","5.7.2015"];
+var disabledTourDays = ["18.4.2015","27.3.2015","28.3.2015","21.3.2015","10.4.2015","11.4.2015","12.4.2015","4.4.2015","5.4.2015","17.4.2015","25.4.2015","26.4.2015","29.5.2015","30.5.2015","31.5.2015","3.7.2015","4.7.2015","5.7.2015"];
+//var disabledTourDays = loadData("//gruseltour-leipzig.de/wordpress/wp-content/themes/gruseltour-leipzig/js/data.txt");
 var disabledText = "Die Tour ist an diesem Tag schon ausgebucht";
