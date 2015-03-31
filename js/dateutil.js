767e58feafce9d554d4602d248f036334da650ad
Sebastian Rothe <creeper_si@yahoo.de>
2015-03-31 14:55:15 +0700

    removed old functions renamed stuff

--- js/dateutil.js
+++ js/dateutil.js
@@ -9,14 +9,6 @@ if (!String.prototype.format) {
 }
 
 function toGermanDateString(date) {
-  return toGermanDateStringWithZeros(date);   
-}
-
-function toGermanDateStringOld(date) {
-  return "{0}.{1}.{2}".format(date.getDate(), date.getMonth() + 1, date.getFullYear()); 
-}
-
-function toGermanDateStringWithZeros(date) {
   return "{0}.{1}.{2}".format(padZero(date.getDate()), padZero(date.getMonth() + 1), date.getFullYear());
 }
 
@@ -29,25 +21,15 @@ function parseGermanDate(dateString) {
   return new Date(parts[2], parts[1] - 1, parts[0]);
 }
 
-function toGermanStringWithZeros(str) {
-	var date = parseGermanDate(str);
-  return "{0}.{1}.{2}".format(padZero(date.getDate()), padZero(date.getMonth() + 1), date.getFullYear());   
-}
-
-function toGermanString(str) {
-  var date = parseGermanDate(str);
-  return "{0}.{1}.{2}".format(date.getDate(), date.getMonth() + 1, date.getFullYear());   
+function stringToGermanDateString(str) {
+  return toGermanDateString(parseGermanDate(str));  
 }
 
 function cleanDisabledDateString(dirtyString) {
 	return dirtyString.trim().replace(/ /g,'').replace(/\r\n/g, '\n');
 }
 
-function isDateDisabledOld(date, disabledDates) {
-    return jQuery.inArray(toGermanDateStringOld(date), disabledDates) != -1;
-}
-
 function transformDateLinesToArray(lines) {
   // clean, split and parseToGerman
-  return cleanDisabledDateString(lines).split("\n").map(toGermanStringWithZeros);
+  return cleanDisabledDateString(lines).split("\n").map(stringToGermanDateString);
 }
