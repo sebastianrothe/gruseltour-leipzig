0d00e7749442d2bcfa24b0b2ab624eeea78b389e
Sebastian <creeper_si@yahoo.de>
2015-06-17 17:43:43 +0200

    Kommentar entfernt

--- js/datepicker.js
+++ js/datepicker.js
@@ -1,5 +1,4 @@
-/* an diesen Tagen können keine Termine mehr angenommen werden */
-//var disabledTourDays = ["18.4.2015","27.3.2015","28.3.2015","21.3.2015","10.4.2015","11.4.2015","12.4.2015","4.4.2015","5.4.2015","17.4.2015","25.4.2015","26.4.2015","29.5.2015","30.5.2015","31.5.2015"];
+// an diesen Tagen können keine Termine mehr angenommen werden
 var disabledTourDays;
 jQuery.get("//gruseltour-leipzig.de/wordpress/wp-content/themes/gruseltour-leipzig/js/data.txt", function(data) { disabledTourDays = transformDateLinesToArray(data); 
 });
