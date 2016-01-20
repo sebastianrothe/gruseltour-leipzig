cbc3d1387503194a0f6079e9979e018db17480cb
Sebastian <sebastian@lifeinprogress.de>
2016-01-21 00:24:46 +0100

    moved functions change namespace refactor util functions to object add timeToString method strict mode

--- js/datepicker/util.js
+++ js/datepicker/util.js
@@ -1,57 +1,65 @@
-// global namespace
-window.gruseltourApp = window.gruseltourApp || {};
+"use strict";
+(function (gruseltourApp) {
+  var augmentStringWithFormat = (function () {
+    if (String.prototype.format) { // First, checks if it isn't implemented yet.
+      return;
+    }
 
-// First, checks if it isn't implemented yet.
-if (!String.prototype.format) {
-  String.prototype.format = function() {
-    var args = arguments;
-    return this.replace(/{(\d+)}/g, function(match, number) { 
-      return typeof args[number] != 'undefined' ? args[number] : match;
-    });
-  };
-}
-
-// package namespace
-window.gruseltourApp.Util = {
-  padZero: function(n) {
-    return n < 10 ? '0' + n : n;
-  },
-
-  toGermanDateString: function(date) {
-    var day = window.gruseltourApp.Util.padZero(date.getDate());
-    var month = window.gruseltourApp.Util.padZero(date.getMonth() + 1);
-    var year = date.getFullYear();
-
-    return "{0}.{1}.{2}".format(day, month, year);
-  },
-
-  parseGermanDate: function(dateString) {
-    var createDate = function(parts) {
-      var day = parts[2], month = parts[1] - 1, year = parts[0];
-      return new Date(day, month, year);
+    String.prototype.format = function () {
+      var args = arguments;
+      return this.replace(/{(\d+)}/g, function (match, number) { 
+        return args[number] !== 'undefined' ? args[number] : match;
+      });
     };
-    return createDate(dateString.split('.'));
-  },
+  }());
 
-  stringToGermanDateString: function(str) {
-    var germanDate = window.gruseltourApp.Util.parseGermanDate(str);
-    return window.gruseltourApp.Util.toGermanDateString(germanDate);
-  },
+  // package namespace
+  gruseltourApp.util = {
+    padZero: function (n) {
+      return n < 10 ? '0' + n : n;
+    },
 
-  cleanDisabledDateString: function(dirtyString) {
-  	return jQuery.trim(dirtyString).replace(/ /g,'').replace(/\r\n/g, '\n');
-  },
+    toGermanDateString: function (date) {
+      var day = gruseltourApp.util.padZero(date.getDate()),
+          month = gruseltourApp.util.padZero(date.getMonth() + 1),
+          year = date.getFullYear();
+      return "{0}.{1}.{2}".format(day, month, year);
+    },
 
-  // clean, split and parseToGerman
-  transformDateLinesToArray: function(lines) { 
-    var getSplittedCleanedLines = function() {
-      var getCleanedLines = function() {
-        return window.gruseltourApp.Util.cleanDisabledDateString(lines);
+    toGermanTimeString: function (date) {
+      var hour = gruseltourApp.util.padZero(date.getHours()),
+          minute = gruseltourApp.util.padZero(date.getMinutes());
+      return "{0}:{1}".format(hour, minute);
+    },
+
+    parseGermanDate: function (dateString) {
+      var createDate = function (parts) {
+        var day = parts[2], 
+            month = parts[1] - 1, 
+            year = parts[0];
+        return new Date(day, month, year);
       };
-      return getCleanedLines().split("\n");
-    };
+      return createDate(dateString.split('.'));
+    },
 
-    var stringToGermanDateString = window.gruseltourApp.Util.stringToGermanDateString;
-    return jQuery.map(getSplittedCleanedLines(), stringToGermanDateString);
-  }
-};
+    stringToGermanDateString: function (str) {
+      var germanDate = gruseltourApp.util.parseGermanDate(str);
+      return gruseltourApp.util.toGermanDateString(germanDate);
+    },
+
+    cleanDisabledDateString: function (dirtyString) {
+      return jQuery.trim(dirtyString).replace(/ /g,'').replace(/\r\n/g, '\n');
+    },
+
+    // clean, split and parseToGerman
+    transformDateLinesToArray: function (lines) { 
+      var getSplittedCleanedLines = function () {
+        var getCleanedLines = function () {
+          return gruseltourApp.util.cleanDisabledDateString(lines);
+        };
+        return getCleanedLines().split("\n");
+      };
+      return jQuery.map(getSplittedCleanedLines(), gruseltourApp.util.stringToGermanDateString);
+    }
+  };
+}(window.gruseltourApp = window.gruseltourApp || {})); // create global namespace and run it
