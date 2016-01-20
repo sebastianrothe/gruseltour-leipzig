d7d92c0ad035e29023a2875ba11d664bcb666cd6
Sebastian <sebastian@lifeinprogress.de>
2016-01-21 00:25:18 +0100

    strict mode show function invocation

--- js/datepicker/dataprovider.js
+++ js/datepicker/dataprovider.js
@@ -1,21 +1,22 @@
-(function(gruseltourApp) {
-	gruseltourApp.dataProvider = function() {
+"use strict";
+(function (gruseltourApp) {
+	gruseltourApp.produceDataProvider = function () {
 		// we will store our days here
 		var disabledTourDays;
 		
-		var load = function() {
+		var load = (function () {
 			/*
 			jQuery.get("//gruseltour-leipzig.de/wordpress/wp-content/themes/gruseltour-leipzig/js/data.txt", function(data) { 
 				disabledTourDays = transformDateLinesToArray(data); 
 			});
 			*/
-			disabledTourDays = gruseltourApp.Util.transformDateLinesToArray("19.1.2016");
-		}(); // run this on loading this object
+			disabledTourDays = gruseltourApp.util.transformDateLinesToArray("19.2.2016");
+		}()); // run this on loading this object
 
 		return {
-			getData: function() {
+			getData: function () {
 				return disabledTourDays;
 			}
 		};
 	};
-}(window.gruseltourApp = window.gruseltourApp || {}));
+}(window.gruseltourApp = gruseltourApp || {}));
