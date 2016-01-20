68e6ebb2f398131cefd810e26fea6c41c6aee63a
Sebastian <sebastian@lifeinprogress.de>
2016-01-21 00:23:42 +0100

    refactor tooltips into object strict mode

--- js/datepicker/availability.js
+++ js/datepicker/availability.js
@@ -1,10 +1,11 @@
-(function(gruseltourApp) {
-	gruseltourApp.dateHelper = function() {
+"use strict";
+(function (gruseltourApp) {
+	gruseltourApp.produceDateHelper = function () {
 		var DATE_FRIDAY = 5, DATE_SATURDAY = 6, DATE_SUNDAY = 0;
 		return {
 			isDisabled: function(date, disabledDates) {
-				var germanDateString = gruseltourApp.Util.toGermanDateString(date);
-		    	return jQuery.inArray(germanDateString, disabledDates) != -1;
+				var germanDateString = gruseltourApp.util.toGermanDateString(date);
+		    	return jQuery.inArray(germanDateString, disabledDates) !== -1;
 			},
 
 			// weekend includes friday
@@ -20,20 +21,44 @@
 		};
 	};
 
-	gruseltourApp.dateChecker = function(dataProvider) {
-		var disabledText = "Die Tour ist an diesem Tag schon ausgebucht";
-		var noRegularTourText = "An diesem Tag findet keine reguläre Tour statt";
-		var disabledDates = dataProvider.getData();
-		var dateHelper = gruseltourApp.dateHelper();
+	gruseltourApp.produceHTMLEntities = function () {
+		var disabledText = "Die Tour ist an diesem Tag schon ausgebucht.",
+			disabledStyleClass = "fully-booked",
+			noRegularTourText = "An diesem Tag findet keine reguläre Tour statt.",
+			noRegularTourStyleClass = "not-available";
 		
 		return {
-			isAvailable: function(date) {
+			getDisabledObject: function () {
+				return {
+					title: disabledText,
+					style: disabledStyleClass
+				};
+			},
+
+			getNoRegularTourObject: function () {
+				return {
+					title: noRegularTourText,
+					style: noRegularTourStyleClass
+				};
+			}
+		};
+	};
+
+	gruseltourApp.produceDateChecker = function (dataProvider) {
+		var disabledDates = dataProvider.getData(),
+			dateHelper = gruseltourApp.produceDateHelper(),
+			htmlEntities = gruseltourApp.produceHTMLEntities(),
+			noRegularTour = htmlEntities.getNoRegularTourObject(),
+			disabledTour = htmlEntities.getDisabledObject();
+
+		return {
+			isAvailable: function (date) {
 				if (dateHelper.isNotWeekend(date)) {
-			        return [false, "not-available", noRegularTourText];
+			        return [false, noRegularTour.style, noRegularTour.title];
 			    }
 
 			    if (dateHelper.isDisabled(date, disabledDates)) {
-			        return [false, "not-available", disabledText];
+			        return [false, disabledTour.style, disabledTour.title];
 			    }
 
 			    return [true];
