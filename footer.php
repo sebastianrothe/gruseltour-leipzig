3a3c1934627943f666e565402b95acd2ac270026
Sebastian <sebastian@lifeinprogress.de>
2016-01-29 23:30:00 +0100

    change pagespeed text

--- footer.php
+++ footer.php
@@ -28,7 +28,7 @@
 			<br />
 			Gruseltour Leipzig made with <i class="fa fa-inverse fa-heart"></i>, <i class="fa fa-inverse fa-coffee"></i> and lots of Open Source in Leipzig.
 			<br />
-			<span class="pagespeed">Generiert mit <?php echo get_num_queries(); ?> Queries in <?php timer_stop(1); ?> Sekunden.</span>
+			<span class="pagespeed">Queries: <?php echo get_num_queries(); ?> Speed: <?php timer_stop(1); ?></span>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
