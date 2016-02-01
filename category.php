2ead761c479efe454d8ae51117f11da939ba22f7
Sebastian <sebastian@lifeinprogress.de>
2016-02-01 19:10:45 +0100

    remove category header

--- category.php
+++ category.php
@@ -17,15 +17,6 @@ get_header(); ?>
 		<div id="content" role="main">
 
 		<?php if ( have_posts() ) : ?>
-			<!-- skip the header
-			<header class="archive-header">
-				<h1 class="archive-title"><?php printf( __( 'Category Archives: %s', 'twentytwelve' ), '<span>' . single_cat_title( '', false ) . '</span>' ); ?></h1>
-
-			<?php if ( category_description() ) : // Show an optional category description ?>
-				<div class="archive-meta"><?php echo category_description(); ?></div>
-			<?php endif; ?>
-			</header><!-- .archive-header -->
-
 			<?php
 			/* Start the Loop */
 			while ( have_posts() ) : the_post();
