776c456dc29cd2ab40b42b2750fb753bc4e5baad
Sebastian Rothe <rothe@infai.org>
2019-01-10 23:30:20 +0100

    add custom image

--- header.php
+++ header.php
@@ -25,6 +25,7 @@
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <meta property="fb:pages" content="173137619558929" />
+<meta property="fb:app_id" content="855961361253886" />
 <?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
@@ -35,14 +36,16 @@
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
 	<header id="masthead" class="site-header" role="banner">
+		<?php if (false) { ?>
 		<hgroup>
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		</hgroup>
-
+		<?php } ?>
+		
 		<!-- switch header and nav -->
 		<?php if ( get_header_image() ) : ?>
-		<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php header_image(); ?>" class="header-image" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" /></a>
+		<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="//res.cloudinary.com/gruseltourleipzig/image/upload/q_auto/f_auto/v1508406596/cropped-cropped-grusel-poster-a3-e1480026424555-1_cgpp1f.jpg" class="header-image" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" /></a>
 		<?php endif; ?>
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
